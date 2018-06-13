afee479c7fe9c353fcb1c23f7b2ec479aa107d6d
Summer <935514540@qq.com>
2018-06-13 15:26:53 +0800

    手动车定位系统

--- new-posys/Action_User/customer/customer.c
+++ new-posys/Action_User/customer/customer.c
@@ -115,25 +115,25 @@ void DataSend(void)
 	valSend.val=(float)allPara.sDta.Result_Angle[2];
   memcpy(tdata+2,valSend.data,4);
 	
-	valSend.val=-(float)allPara.sDta.vellx;
+	valSend.val=(float)allPara.sDta.vellx;
   memcpy(tdata+6,valSend.data,4);
 	
-	valSend.val=-(float)allPara.sDta.velly;
+	valSend.val=(float)allPara.sDta.velly;
   memcpy(tdata+10,valSend.data,4);
 	
-	valSend.val=-(float)allPara.sDta.posx;
+	valSend.val=(float)allPara.sDta.posx;
   memcpy(tdata+14,valSend.data,4);
 	 
-	valSend.val=-(float)allPara.sDta.posy;
+	valSend.val=(float)allPara.sDta.posy;
   memcpy(tdata+18,valSend.data,4);
 	 
 	valSend.val=(float)allPara.GYRO_Real[2];
   memcpy(tdata+22,valSend.data,4);
 	
-	valSend.val=(float)(4096-allPara.sDta.codeData[0]);
+	valSend.val=(float)(allPara.sDta.codeData[0]);
   memcpy(tdata+26,valSend.data,4);
 	 
-	valSend.val=(float)(4096-allPara.sDta.codeData[1]);
+	valSend.val=(float)(allPara.sDta.codeData[1]);
   memcpy(tdata+30,valSend.data,4);
 //	
 	#ifdef TEST_SUMMER
